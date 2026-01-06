64fe93e286280296c360091b2691057013c22007
Anton <anton@dotdotdot.com.au>
2026-01-06 14:06:45 +1000

    Updated dependencies Allow export to unprotected pdf

--- PdfPassword/Program.cs
+++ PdfPassword/Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using iText;
+using iText.Kernel.Exceptions;
+using iText.Kernel.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using iTextSharp.text.exceptions;
-using iTextSharp.text.pdf;
 
 namespace PdfPassword
 {
@@ -18,6 +20,8 @@ namespace PdfPassword
             if (args == null || args.Length == 0) return;
 
             var pdfPath = args[0];
+            
+            var destinationPath = (args.Length > 1) ? args[1] : null ;
 
             var stopwatch = new Stopwatch();
 
@@ -34,18 +38,24 @@ namespace PdfPassword
             }
 
             stopwatch.Start();
-
-            Find(pdfPath);
+            
+            var password = Find(pdfPath);
             
             stopwatch.Stop();
 
             Console.WriteLine("Completed in.... {0} minutes {1} seconds", stopwatch.Elapsed.Minutes,stopwatch.Elapsed.Seconds);
 
             Console.WriteLine("Completed in.... {0} seconds", stopwatch.Elapsed.TotalSeconds);
+
+
+            if (!string.IsNullOrEmpty(destinationPath))
+            {
+                SavePdf(pdfPath, password, destinationPath);
+            }
         }
 
 
-        public static void Find(string pdfFullname)
+        public static string Find(string pdfFullname)
         {
             for (int y = 1940; y < 2001; y++)
             {
@@ -55,14 +65,18 @@ namespace PdfPassword
                     {
                         var password = string.Format("{0}{1}{2}", d.ToString("00"), m.ToString("00"), y);
 
-                        if (IsPasswordValid(pdfFullname, GetBytes(password)))
+                        //if (IsPasswordValid(pdfFullname, password))
+                        if (IsPasswordValid(pdfFullname, Encoding.UTF8.GetBytes(password)))
                         {
                             Console.WriteLine("Correct password is: {0}", password);
-                            return;
+                            return password;
                         }
                     }
                 }
-            }          
+            }
+
+            return null;
+
         }
 
         public static void FindParallel(string pdfFullname)
@@ -87,7 +101,33 @@ namespace PdfPassword
         {
             try
             {
-                var pdfReader = new PdfReader(pdfFullname);
+
+                using (PdfDocument document = new PdfDocument(new PdfReader(pdfFullname)))
+                {
+
+                    int numberOfPdfObject = document.GetNumberOfPdfObjects();
+
+                    for (int i = 1; i <= numberOfPdfObject; i++)
+                    {
+                        var obj = document.GetPdfObject(i);
+                        if (obj != null && obj.IsStream())
+                        {
+                            byte[] b;
+                            try
+                            {
+
+                                // Get decoded stream bytes.
+                                b = ((PdfStream)obj).GetBytes();
+                            }
+                            catch (PdfException)
+                            {
+
+                                return true;
+                            } 
+                        }
+                    }
+                }
+                
                 return false;
             }
             catch (BadPasswordException)
@@ -101,8 +141,44 @@ namespace PdfPassword
         {
             try
             {
-                var pdfReader = new PdfReader(pdfFullname, password);
-                return true;
+                using (PdfDocument document = 
+                        new PdfDocument(new PdfReader(pdfFullname, 
+                                                    new ReaderProperties().SetPassword(password)).SetUnethicalReading(true))
+                      )
+                {
+
+                    return document.GetReader().IsOpenedWithFullPermission();
+                }                
+            }
+            catch (BadPasswordException)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsPasswordValid(string pdfFullname, string password)
+        {
+            try
+            {
+
+                var userPassword = string.Empty;
+
+                using (PdfDocument document = 
+                            new PdfDocument(new PdfReader(pdfFullname, 
+                                new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(password))).SetUnethicalReading(true)
+                            ))
+                {
+                    byte[] computeUserPassword = document.GetReader().ComputeUserPassword();
+
+                    // The result of user password computation logic can be null in case of
+                    // AES256 password encryption or non password encryption algorithm
+                    userPassword = computeUserPassword == null ? null : Encoding.UTF8.GetString(computeUserPassword);
+                    Console.Out.WriteLine(password);
+                }
+
+
+                return (!string.IsNullOrEmpty(userPassword));
+
             }
             catch (BadPasswordException)
             {
@@ -113,6 +189,20 @@ namespace PdfPassword
         private static byte[] GetBytes(string str)
         {
             return Encoding.ASCII.GetBytes(str);           
+        }
+
+        private static void SavePdf(string source, string password, string destination)
+        {
+
+            FileInfo file = new FileInfo(destination);
+            file.Directory.Create();
+            
+            var pdfReader = new PdfReader(source, new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(password))).SetUnethicalReading(true);
+
+            var pdfWriter = new PdfWriter(destination);
+
+            var pdfDoc = new PdfDocument(pdfReader, pdfWriter);
+            pdfDoc.Close();
         }
     }
 }
